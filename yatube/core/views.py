1a2ad6e87b92f0e45307a38cd72dee2c77f62e34
Ural Ataullin <ataullinural@gmail.com>
2023-01-18 15:04:21 +0500

    custom_pages

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def page_not_found(request, exception):
     """Переопределение страницы с ошибкой 404."""
     return render(request, 'core/404.html', {'path': request.path}, status=404)
+
+
+def csrf_failure(request, reason=''):
+    """Переопределение страницы с ошибкой 403csrf."""
+    return render(request, 'core/403csrf.html')
--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -146,3 +146,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.LimitOffsetPagination',
 }
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
